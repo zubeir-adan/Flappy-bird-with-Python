3b5ec474b361ae4f4c9bbe9d56846615cc7e704d
zubeir-adan <zubeir.adan@strathmore.edu>
2024-11-08 05:53:03 +0300

    Variables

--- main.py
+++ main.py
@@ -2,3 +2,16 @@ import pygame
 import random
 import cairo
 import math
+
+
+
+# Variables
+SCREEN_WIDTH = 400
+SCREEN_HEIGHT = 600
+SPEED = 10
+GRAVITY = 1
+GAME_SPEED = 5
+PIPE_WIDTH = 80 
+PIPE_GAP = 150
+BIRD_RADIUS = 20
+LIVES = 3
