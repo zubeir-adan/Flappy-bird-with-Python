fadfb08ff139b95e36890f1d0b7033fc7789eda0
zubeir-adan <zubeir.adan@strathmore.edu>
2024-11-08 05:53:51 +0300

    Initializing pygame

--- main.py
+++ main.py
@@ -15,3 +15,12 @@ PIPE_WIDTH = 80
 PIPE_GAP = 150
 BIRD_RADIUS = 20
 LIVES = 3
+
+
+
+# Initialize Pygame
+pygame.init()
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('3D Flappy Sphere')
+clock = pygame.time.Clock()
+font = pygame.font.SysFont(None, 30)
