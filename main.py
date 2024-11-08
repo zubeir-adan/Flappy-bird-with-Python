beab056bb2c003d9eb0b0bef375440e1c619132b
zubeir-adan <zubeir.adan@strathmore.edu>
2024-11-08 05:56:20 +0300

    Class Pipe

--- main.py
+++ main.py
@@ -57,3 +57,45 @@ class Bird:
         ball_surface = pygame.image.frombuffer(surface.get_data(), (2 * BIRD_RADIUS, 2 * BIRD_RADIUS), "ARGB")
         screen.blit(ball_surface, (self.x - BIRD_RADIUS, int(self.y) - BIRD_RADIUS))
 
+# Pipe Class with shading for 3D effect
+class Pipe:
+    def __init__(self, x, height, inverted):
+        self.x = x 
+        self.width = PIPE_WIDTH
+        self.height = height
+        self.inverted = inverted
+
+    def update(self):
+        self.x -= GAME_SPEED
+
+    def draw(self, screen):
+        # Use Cairo for shading effect on the pipes
+        surface = cairo.ImageSurface(cairo.FORMAT_ARGB32, PIPE_WIDTH, SCREEN_HEIGHT)
+        cr = cairo.Context(surface)
+        
+        # Draw pipe with shading for 3D effect
+        cr.rectangle(0, 0, PIPE_WIDTH, SCREEN_HEIGHT)
+        gradient = cairo.LinearGradient(0, 0, PIPE_WIDTH, 0)
+        gradient.add_color_stop_rgba(0, 140.2, 140.8, 140.2, 1)  # Left side
+        gradient.add_color_stop_rgba(1, 255, 255, 0.8, 1)      # Right side
+        cr.set_source(gradient)
+        cr.fill()
+
+        # Convert Cairo surface to Pygame surface
+        pipe_surface = pygame.image.frombuffer(surface.get_data(), (PIPE_WIDTH, SCREEN_HEIGHT), "ARGB")
+
+        # Draw pipe in the correct position
+        if self.inverted:
+            screen.blit(pipe_surface, (self.x, 0), (0, SCREEN_HEIGHT - self.height, PIPE_WIDTH, self.height))
+        else:
+            screen.blit(pipe_surface, (self.x, SCREEN_HEIGHT - self.height))
+
+    def collide(self, bird):
+        if bird.x + BIRD_RADIUS > self.x and bird.x - BIRD_RADIUS < self.x + self.width:
+            if self.inverted and bird.y - BIRD_RADIUS < self.height:
+                return True
+            if not self.inverted and bird.y + BIRD_RADIUS > SCREEN_HEIGHT - self.height:
+                return True
+        return False
+
+
